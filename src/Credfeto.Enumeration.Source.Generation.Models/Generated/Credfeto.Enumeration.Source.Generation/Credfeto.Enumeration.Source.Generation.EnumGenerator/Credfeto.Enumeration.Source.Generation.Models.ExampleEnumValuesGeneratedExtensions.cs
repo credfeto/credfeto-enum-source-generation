52f54a274975bd51b9739aa261b1793a4ad834b3
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-11-06 14:37:39 +0000

    [FF-1429] Updating Meziantou.Analyzer (Code Analysis) to 1.0.745

---
+++ src/Credfeto.Enumeration.Source.Generation.Models/Generated/Credfeto.Enumeration.Source.Generation/Credfeto.Enumeration.Source.Generation.EnumGenerator/Credfeto.Enumeration.Source.Generation.Models.ExampleEnumValuesGeneratedExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Credfeto.Enumeration.Source.Generation.Models;
+
+[GeneratedCode(tool: "Credfeto.Enumeration.Source.Generation.EnumGenerator", version: "0.0.0.1-do-not-distribute")]
+public static class ExampleEnumValuesGeneratedExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string GetName(this ExampleEnumValues value)
+    {
+        return value switch
+        {
+            ExampleEnumValues.ZERO => "ZERO",
+            ExampleEnumValues.ONE => "ONE",
+            ExampleEnumValues.THREE => "THREE",
+            _ => throw new ArgumentOutOfRangeException(nameof(value), actualValue: value, message: "Unknown enum member")
+        };
+        
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string GetDescription(this ExampleEnumValues value)
+    {
+        return value switch
+        {
+            ExampleEnumValues.ONE => "One \"1\"",
+            ExampleEnumValues.THREE => "Two but one better!",
+            _ => GetName(value)
+        };
+        
+    }
+    
+}
+
