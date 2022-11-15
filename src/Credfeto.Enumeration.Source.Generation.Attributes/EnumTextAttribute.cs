c8bafeb10f01d9a441a60db5a7dee6cdb7e93654
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-11-15 23:51:32 +0000

    Added check for type

--- src/Credfeto.Enumeration.Source.Generation.Attributes/EnumTextAttribute.cs
+++ src/Credfeto.Enumeration.Source.Generation.Attributes/EnumTextAttribute.cs
@@ -5,9 +5,14 @@ namespace Credfeto.Enumeration.Source.Generation.Attributes;
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
 public sealed class EnumTextAttribute : Attribute
 {
-    public EnumTextAttribute(Type @enum)
+    public EnumTextAttribute(Type enumType)
     {
-        this.Enum = @enum;
+        this.Enum = enumType;
+
+        if (!enumType.IsEnum)
+        {
+            throw new ArgumentException(message: "The type must be an enum.", nameof(enumType));
+        }
     }
 
     public Type Enum { get; }
