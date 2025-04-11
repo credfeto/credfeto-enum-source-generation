eb8d555d3d9af3ea25b1eeb09448786624700a91
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-11 18:19:28 +0100

    Validate enum type with exception raising in a different method

--- src/Credfeto.Enumeration.Source.Generation.Attributes/EnumTextAttribute.cs
+++ src/Credfeto.Enumeration.Source.Generation.Attributes/EnumTextAttribute.cs
@@ -7,13 +7,20 @@ public sealed class EnumTextAttribute : Attribute
 {
     public EnumTextAttribute(Type enumType)
     {
-        this.Enum = enumType;
-
-        if (!enumType.IsEnum)
-        {
-            throw new ArgumentException(message: "The type must be an enum.", nameof(enumType));
-        }
+        this.Enum = ValidateEnumType(enumType);
     }
 
     public Type Enum { get; }
+
+    private static Type ValidateEnumType(Type enumType)
+    {
+        return enumType.IsEnum
+            ? enumType
+            : NotAnEnum(enumType);
+    }
+
+    private static Type NotAnEnum(Type enumType)
+    {
+        throw new ArgumentException($"The type ({enumType.FullName}) must be an enum.", nameof(enumType));
+    }
 }
