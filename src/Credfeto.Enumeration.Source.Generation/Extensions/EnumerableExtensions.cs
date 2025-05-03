0a1b9f741a0beeefeafacf7912bdc3858194c366
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-05-03 12:05:28 +0000

    Cleanup: EnumerableExtensions.cs

--- src/Credfeto.Enumeration.Source.Generation/Extensions/EnumerableExtensions.cs
+++ src/Credfeto.Enumeration.Source.Generation/Extensions/EnumerableExtensions.cs
@@ -7,8 +7,6 @@ internal static class EnumerableExtensions
 {
     public static IEnumerable<T> RemoveNulls<T>(this IEnumerable<T?> source)
     {
-        return from item in source
-               where item is not null
-               select item;
+        return from item in source where item is not null select item;
     }
 }
