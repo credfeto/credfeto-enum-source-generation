aa2700df48d0f798d9ecf8766a9e72f07a66be1f
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-10-17 21:30:15 +0000

    Cleanup: EnumSourceGenerator.cs

--- src/Credfeto.Enumeration.Source.Generation/EnumSourceGenerator.cs
+++ src/Credfeto.Enumeration.Source.Generation/EnumSourceGenerator.cs
@@ -15,10 +15,13 @@ public static class EnumSourceGenerator
 
         source = AddUsingDeclarations(new CodeBuilder().AppendFileHeader());
 
-        using (source.AppendLine("namespace " + enumDeclaration.Namespace + ";")
-                     .AppendBlankLine()
-                     .AppendGeneratedCodeAttribute()
-                     .StartBlock(enumDeclaration.AccessType.ConvertAccessType() + " static class " + className))
+        using (
+            source
+                .AppendLine("namespace " + enumDeclaration.Namespace + ";")
+                .AppendBlankLine()
+                .AppendGeneratedCodeAttribute()
+                .StartBlock(enumDeclaration.AccessType.ConvertAccessType() + " static class " + className)
+        )
         {
             ClassNameOnlyFormatter classNameOnlyFormatter = new(enumDeclaration);
 
@@ -30,14 +33,22 @@ public static class EnumSourceGenerator
 
     private static CodeBuilder AddUsingDeclarations(CodeBuilder source)
     {
-        return AddUsingDeclarations(source: source, "System", "System.CodeDom.Compiler", "System.Diagnostics", "System.Diagnostics.CodeAnalysis", "System.Runtime.CompilerServices");
+        return AddUsingDeclarations(
+            source: source,
+            "System",
+            "System.CodeDom.Compiler",
+            "System.Diagnostics",
+            "System.Diagnostics.CodeAnalysis",
+            "System.Runtime.CompilerServices"
+        );
     }
 
     private static CodeBuilder AddUsingDeclarations(CodeBuilder source, params string[] namespaces)
     {
-        return namespaces.OrderBy(keySelector: n => n, comparer: StringComparer.OrdinalIgnoreCase)
-                         .Aggregate(seed: source, func: (current, ns) => current.AppendLine($"using {ns};"))
-                         .AppendBlankLine();
+        return namespaces
+            .OrderBy(keySelector: n => n, comparer: StringComparer.OrdinalIgnoreCase)
+            .Aggregate(seed: source, func: (current, ns) => current.AppendLine($"using {ns};"))
+            .AppendBlankLine();
     }
 
     public static string GenerateClassForClass(in ClassEnumGeneration classDeclaration, out CodeBuilder source)
@@ -45,38 +56,47 @@ public static class EnumSourceGenerator
         string className = classDeclaration.Name;
 
         source = AddUsingDeclarations(new CodeBuilder().AppendFileHeader())
-                 .AppendLine($"namespace {classDeclaration.Namespace};")
-                 .AppendBlankLine()
-                 .AppendGeneratedCodeAttribute();
+            .AppendLine($"namespace {classDeclaration.Namespace};")
+            .AppendBlankLine()
+            .AppendGeneratedCodeAttribute();
 
         using (source.StartBlock($"{classDeclaration.AccessType.ConvertAccessType()} static partial class {className}"))
         {
             bool isFirst = true;
 
-            source = classDeclaration.Enums.Aggregate(seed: source,
-                                                      func: (builder, enumGeneration) =>
-                                                            {
-                                                                try
-                                                                {
-                                                                    return GenerateMethodsForEnum(builder: builder, enumGeneration: enumGeneration, isFirst: isFirst);
-                                                                }
-                                                                finally
-                                                                {
-                                                                    isFirst = false;
-                                                                }
-                                                            });
+            source = classDeclaration.Enums.Aggregate(
+                seed: source,
+                func: (builder, enumGeneration) =>
+                {
+                    try
+                    {
+                        return GenerateMethodsForEnum(
+                            builder: builder,
+                            enumGeneration: enumGeneration,
+                            isFirst: isFirst
+                        );
+                    }
+                    finally
+                    {
+                        isFirst = false;
+                    }
+                }
+            );
         }
 
         return className;
     }
 
-    private static CodeBuilder GenerateMethodsForEnum(CodeBuilder builder, in EnumGeneration enumGeneration, bool isFirst)
+    private static CodeBuilder GenerateMethodsForEnum(
+        CodeBuilder builder,
+        in EnumGeneration enumGeneration,
+        bool isFirst
+    )
     {
         IFormatConfig formatConfig = new ClassWithNamespaceFormatter(enumGeneration);
 
         return isFirst
-            ? builder.AppendBlankLine()
-                     .GenerateMethods(attribute: enumGeneration, formatConfig: formatConfig)
+            ? builder.AppendBlankLine().GenerateMethods(attribute: enumGeneration, formatConfig: formatConfig)
             : builder.GenerateMethods(attribute: enumGeneration, formatConfig: formatConfig);
     }
 }
