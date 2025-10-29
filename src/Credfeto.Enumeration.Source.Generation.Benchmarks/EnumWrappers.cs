2caa7b415167f41da96becd47d7da2d9b7100640
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-10-29 23:54:17 +0000

    Cleanup: EnumWrappers.cs

--- src/Credfeto.Enumeration.Source.Generation.Benchmarks/EnumWrappers.cs
+++ src/Credfeto.Enumeration.Source.Generation.Benchmarks/EnumWrappers.cs
@@ -7,7 +7,7 @@ namespace Credfeto.Enumeration.Source.Generation.Benchmarks;
 
 internal static class EnumWrappers
 {
-    [SuppressMessage(category: "ReSharper", checkId: "InvokeAsExtensionMethod", Justification = "This is a benchmark.")]
+    
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string GetNameReflectionCached<T>(this T value)
         where T : Enum
@@ -15,7 +15,7 @@ internal static class EnumWrappers
         return EnumHelpers.GetName(value);
     }
 
-    [SuppressMessage(category: "ReSharper", checkId: "InvokeAsExtensionMethod", Justification = "This is a benchmark.")]
+    
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string GetDescriptionReflectionCached<T>(this T value)
         where T : Enum
@@ -23,7 +23,7 @@ internal static class EnumWrappers
         return EnumHelpers.GetDescription(value);
     }
 
-    [SuppressMessage(category: "ReSharper", checkId: "InvokeAsExtensionMethod", Justification = "This is a benchmark.")]
+    
     [SuppressMessage(
         category: "Philips.CodeAnalysis.MaintainabilityAnalyzers",
         checkId: "PH2073: Call as instance",
