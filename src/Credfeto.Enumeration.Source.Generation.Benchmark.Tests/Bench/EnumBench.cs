065b3aef99e9be7d473683ad35c2f0b5be7d66d3
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-12-17 13:01:48 +0000

    Cleanup: EnumBench.cs

--- src/Credfeto.Enumeration.Source.Generation.Benchmark.Tests/Bench/EnumBench.cs
+++ src/Credfeto.Enumeration.Source.Generation.Benchmark.Tests/Bench/EnumBench.cs
@@ -9,7 +9,7 @@ namespace Credfeto.Enumeration.Source.Generation.Benchmark.Tests.Bench;
 [SimpleJob]
 [MemoryDiagnoser(false)]
 [SuppressMessage(category: "codecracker.CSharp", checkId: "CC0091:MarkMembersAsStatic", Justification = "Benchmark")]
-[SuppressMessage(category: "Microsoft.Performance", checkId: "CA1822:Mark methods static", Justification = "Benchmark")]
+
 [SuppressMessage(
     category: "FunFair.CodeAnalysis",
     checkId: "FFS0012: Make sealed static or abstract",
